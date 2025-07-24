32b65506631fa6afd881fc00f7c930f3c94925c1
jsult <jsultan2026@spyponders.com>
2025-07-24 09:30:18 -0400

    last minute npc

---
+++ NPC/joseph.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var dialog_timeline: String = "res://Timelines/joseph.dtl"
+var player_in_range = false
+
+func _ready():
+	$Label.visible = false
+
+func _process(_delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("Interact"):
+		Dialogic.start(dialog_timeline)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "JoseCharacterBody2D":
+		player_in_range = true
+		$Label.visible = true
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "JoseCharacterBody2D":
+		player_in_range = false
+		$Label.visible = false
